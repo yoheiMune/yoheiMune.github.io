d95b43f3affe7986a3a9fc18646c41a1ff6b7067
Yohei Munesada <y.munesada@gmail.com>
2015-06-06 00:28:09 +0900

    add push sample

--- push-notification/main.js
+++ push-notification/main.js
@@ -55,6 +55,7 @@ function initializeState () {
         console.debug('serviceWorker ready');
         // Do we already have a push message subscription?
         serviceWorkerRegistration.pushManager.getSubscription().then(function (subscription) {
+            console.debug('aaaaaaaa: ', subscription);
             // Enable any UI which subscribes / unsubscribes
             // from push messages.
             var pushButton = document.querySelector('.js-push-button');
@@ -79,6 +80,94 @@ function initializeState () {
         });
     });
 }
+
+
+function subscribe() {
+    // Disable the button so it can't be changed
+    // while we process the permission request.
+    var pushButton = document.querySelector('.js-push-button');
+    pushButton.disabled = true;
+
+    navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+
+        serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true}).then(function (subscription) {
+            // The subscription was successful
+            isPushEnabled = true;
+            pushButton.textContent = 'Disable Push Messages';
+            pushButton.disabled = false;
+
+            // TODO: Send the subscription subscription.endpoint
+            // to your server and save it to send a push message
+            // at a later date.
+            return sendSubscriptionToServer(subscription);            
+        });
+    
+    }).catch(function (e) {
+        if (Notification.permission === 'ddenied') {
+            // The user denied the notification permission which
+            // means we failed to subscribe and the user will need
+            // to manually change the notification permisson to 
+            // subscribe to push messages
+            console.debug('permisson for Notifications was defined.');
+            pushButton.disabled = true;
+        } else {
+            // A problem occurred with the subscription, this can
+            // often be down to an issue or lack of the gcm_sender_id
+            // and/or gcm_user_visible_only
+            console.debug('Unable to subscribe to push.', e);
+            pushButton.disabled = false;
+            pushButton.textContent = 'Enable Push Messages';
+        }
+    });
+}
+
+// This method handles the removal of subscriptionId
+// in Chrome 44 by concatenating the subscription Id
+// to the subscription endpoint
+function sendSubscriptionToServer (pushSubscription) {
+    console.debug('bbbbbbbbb');
+    // Make sure we only mess with GCM
+    if (pushSubscription.endpoint.indexOf('https://android.googleapis.com/gcm/send') !== 0)  {
+        console.debug('endpoint: ', mergedEndpoint);
+        return pushSubscription.endpoint;
+    }
+
+    var mergedEndpoint = pushSubscription.endpoint;
+    // Chrome 42 + 43 will not have the subscriptionId attached
+    // to the endpoint
+    if (pushSubscription.subscriptionId && pushSubscription.endpoint.indexOf(pushSubscription.subscriptionId) === -1) {
+        // Handle version 42 where you have separate subId and Endpoint
+        mergedEndpoint = pushSubscription.endpoint + '/' + pushSubscription.subscriptionId;
+    }
+
+    console.debug('endpoint: ', mergedEndpoint);
+
+    return mergedEndpoint;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ push-notification/sw.js
@@ -0,0 +1,39 @@
+
+self.addEventListener('push', function (event) {
+    console.log('Received a push message', event);
+
+    var title = 'Yay a message.';
+    var body = 'We have received a push message.';
+    var icon = '/images/icon-192x192.png';
+    var tag = 'simple-push-demo-notification-tag';
+
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: body,
+            icon: icon,
+            tag: tag
+        })
+    );    
+});
+
+
+self.addEventListener('notificationclick', function(evt) {
+    console.log('notificationclick: ', evt);
+
+  evt.notification.close();
+
+  evt.waitUntil(
+    clients.matchAll({ type: 'window' }).then(function(evt) {
+      var p = location.pathname.split('/');
+      p.pop();
+      p = location.protocol + '//' + location.hostname + (location.port ? ':'+location.port : '') + p.join('/') + '/';
+      for(var i = 0 ; i < evt.length ; i++) {
+        var c = evt[i];
+        if(((c.url == p) || (c.url == p + 'index.html')) && ('focus' in c))
+          return c.focus();
+      }
+      if(clients.openWindow)
+        return clients.openWindow('./');
+    })
+  );
+});
